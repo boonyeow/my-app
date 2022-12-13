b0057285a24fb099529b2a10f5ef6927080096ce
BY <9499796+boonyeow@users.noreply.github.com>
2022-12-14 02:54:28 +0800

    test-app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { WalletKitProvider } from "@mysten/wallet-kit";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletKitProvider>
+      <Component {...pageProps} />
+    </WalletKitProvider>
+  );
 }
